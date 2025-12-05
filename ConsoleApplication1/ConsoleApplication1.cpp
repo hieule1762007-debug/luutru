78b52a498da9c652987a216500d6dcdee84ac938
hieule <hieule1762007@gmail.com>
2025-12-05 18:15:24 +0700

    test xoa

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -20,6 +20,21 @@ int main()
     std::cout << "test lan 188489289\n";
     std::cout << "test lan 188489289\n";
     std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
+    std::cout << "test lan 188489289\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
